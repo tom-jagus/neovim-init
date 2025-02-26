8eeefacee94fb4b932f956799be19e28e6430a6e
Tom Jagus <168629725+tom-jagus@users.noreply.github.com>
2025-02-26 01:24:57 +0100

    Modified plugins/core

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,7 +1,33 @@
 return {
-  "LazyVim/LazyVim",
-  opts = {
-    colorscheme = "catppuccin",
-    transparent_background = true,
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+      transparent_background = true,
+    },
+  },
+  {
+    "catppuccin/nvim",
+    opts = {
+      transparent_background = true,
+    },
+  },
+  {
+    "snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          header = [[
+            M"""""""`YM MM""""""""`M MMP"""""YMM M""MMMMM""M M""M M"""""`'"""`YM 
+            M  mmmm.  M MM  mmmmmmmM M' .mmm. `M M  MMMMM  M M  M M  mm.  mm.  M 
+            M  MMMMM  M M`      MMMM M  MMMMM  M M  MMMMP  M M  M M  MMM  MMM  M 
+            M  MMMMM  M MM  MMMMMMMM M  MMMMM  M M  MMMM' .M M  M M  MMM  MMM  M 
+            M  MMMMM  M MM  MMMMMMMM M. `MMM' .M M  MMP' .MM M  M M  MMM  MMM  M 
+            M  MMMMM  M MM        .M MMb     dMM M     .dMMM M  M M  MMM  MMM  M 
+            MMMMMMMMMMM MMMMMMMMMMMM MMMMMMMMMMM MMMMMMMMMMM MMMM MMMMMMMMMMMMMM 
+          ]],
+        },
+      },
+    },
   },
 }
